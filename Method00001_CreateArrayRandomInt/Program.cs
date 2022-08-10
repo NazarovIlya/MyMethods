51bc7ee64a949070b2876f06bcd997b8fd9d257b
NazarovIlya <eliakohai@gmail.com>
2022-08-10 22:58:16 +0300

    CreateArrayRandomInt

---
+++ Method00001_CreateArrayRandomInt/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int[] CreateArrayRandomInt(int arrayLength, int minIntervalValue, int maxIntervalValue)
+{
+    Random rnd = new Random();
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minIntervalValue, maxIntervalValue + 1);
+    }
+    return array;
+}
