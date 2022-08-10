2449214afb32721ccd2b87aade8e812a8e9d4342
NazarovIlya <eliakohai@gmail.com>
2022-08-10 23:17:24 +0300

    FillArrayRandomInt

---
+++ Method00002_FillArrayRandomInt/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+void FillArrayRandomInt(int[] array, int minIntervalValue, int maxIntervalValue)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minIntervalValue, maxIntervalValue + 1);
+    }
+}
