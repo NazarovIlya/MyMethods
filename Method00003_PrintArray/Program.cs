6297c7950ef1380e4dcbe634f03885b7fa8b33fc
NazarovIlya <eliakohai@gmail.com>
2022-08-10 23:41:44 +0300

    PrintArray

---
+++ Method00003_PrintArray/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+void PrintArray(int[] array, string preOutputText, string postOutputText)
+{
+    Console.Write(preOutputText);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}] ");
+    }
+    Console.WriteLine(postOutputText);
+}
