464c61834663f7c448d7ed779fa09dabcc7b3919
NazarovIlya <eliakohai@gmail.com>
2022-08-11 11:05:47 +0300

    corrected argument

--- Method00000_GetUserInputInt/Program.cs
+++ Method00000_GetUserInputInt/Program.cs
@@ -2,7 +2,7 @@
 
 int GetUserInputInt(string userInputString)
 {
-    Console.WriteLine(userInputStr);
+    Console.WriteLine(userInputString);
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
 }
