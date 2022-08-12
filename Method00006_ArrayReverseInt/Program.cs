9f1399ca64406934f24f404bb698a51152d55a5a
NazarovIlya <eliakohai@gmail.com>
2022-08-13 01:57:40 +0300

    ArrayReverseInt

---
+++ Method00006_ArrayReverseInt/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+void ArrayReverse(int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2)
+    {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+        index1++;
+        index2--;
+    }
+}
