71510d3e49f4321bebda99e3bbfca1f7730ad9ce
NazarovIlya <eliakohai@gmail.com>
2022-08-13 01:53:34 +0300

    CreateRandomMatrixInt

---
+++ Method00005_CreateRandomMatrixInt/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int[,] CreateMatrixTwoDimension(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max);
+        }
+    }
+    return matrix;
+}
