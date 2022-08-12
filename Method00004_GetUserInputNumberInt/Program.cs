97a88f3de95a541c7cf5144099c8630111377710
NazarovIlya <eliakohai@gmail.com>
2022-08-13 01:33:43 +0300

    GetUserInputNumbersInt

---
+++ Method00004_GetUserInputNumberInt/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int[] GetUserInputNumbersInt(string userInputNumberString)
+{
+    Console.WriteLine(userInputNumberString);
+    string[] stringArray = Console.ReadLine().Split(",");
+    int[] numberArray = new int[stringArray.Length];
+    for (int i = 0; i < numberArray.Length; i++)
+    {
+        numberArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return numberArray;
+}
