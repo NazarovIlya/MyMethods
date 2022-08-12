68bc68e8753f6be7eea3f66890bc0d27c0f17663
NazarovIlya <eliakohai@gmail.com>
2022-08-13 02:01:01 +0300

    PrintMatrixTwoDimensionalInt

---
+++ Method00007_PrintMatrixTwoDimensionalInt/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+void PrintMatrixTwoDimensionalInt(int[,] matrixTwoDimensional, string userOutputString)
+{
+    Console.WriteLine(userOutputString);
+    for (int i = 0; i < matrixTwoDimensional.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < matrixTwoDimensional.GetLength(1); j++)
+        {
+            if (matrixTwoDimensional[i, j] >= 0)
+                Console.Write($" {matrixTwoDimensional[i, j]} ");
+            else Console.Write($"{matrixTwoDimensional[i, j]} ");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
