35b0bf3ac8eea6cef017e1c80e33e4bfd99bb80a
NazarovIlya <eliakohai@gmail.com>
2022-08-13 11:51:08 +0300

    FillMatrix2dRandomInt

---
+++ Method00008_FillMatrix2DRandomInt/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int[,] FillMatrixTwoDimension(int[,] matrix, int min, int max)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(min, max);
+        }
+    }
+    return matrix;
+}
